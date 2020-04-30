7d92abbb2758e9f2eb17b9949ebfc1d4dd2fc64f
Ömer Can Koç <63169919+omercankoc@users.noreply.github.com>
2020-04-30 16:58:20 +0300

    Update Nullability.kt
    Complated.

--- KotlinTutorials/Nullability.kt
+++ KotlinTutorials/Nullability.kt
@@ -1,31 +1,28 @@
 // Nullability: Bos birakilabilir degisken.
-// var VeriableName : VariableType = null    
+// var VeriableName : VariableType? = null
+// "?" Degiskenin degeri initialize edilecek anlamina gelir.
 // Bu degerler deger atamasi yapilmadan kullanilabiller. 
 // Fakat bu degerler ile bir islem yapılmaya kalktiginda hata alinir.
 // Bu hatalarin onune gecilmesi icin "variableName!!" seklinde kullanilabilir.
-// "!!" Degiskenin degeri kesinlikle initialize edilecek anlamina gelir. (Safe Call)
+// "!!" islevi "Safe Call" olarak adlandirilir.
 // println(VariableName!!*2)
 
-class User(var name : String?, var surname : String?){
-    
+class User(var name : String?, var surname : String?){    
     fun message(name : String, surname : String ) : String {
         return "Hello $name $surname!"
     }
 }
 
 fun main() {
-  
-  // Nullable Safety
-	var user = User("abc","cba")
-  if(first.name != " "){
-    var message : String = user.message(user.name!!, user.surname!!)
-      println(message)
+    var user = User("abc","cba")
+    if(user.name != " "){
+    	var message : String = user.message(user.name!!, user.surname!!)
+    	println(message)
     }
     else {
-      println("Nullability!")
+    	println("Nullable Safety!")
     }
     
-    // Elvis
-    var output : String = user.message(user.name!!,user.surname!!) ?: "Nullability"
+    var output : String = user.message(user.name!!,user.surname!!) ?: "Elvis"
     println(output)
 }
